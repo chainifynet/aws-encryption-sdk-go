7b3402a302ead58a6fbc5cd3e2831151ab25c60b
wobondar <wobondar@gmail.com>
2023-12-01 21:14:11 +0200

    feat(util): gen combinations tool

---
+++ pkg/helpers/itertools/comb.go
@@ -0,0 +1,51 @@
+// Copyright Chainify Group LTD. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package itertools provides a method to generate all combinations out of a given string array.
+package itertools
+
+import "math/bits"
+
+// Combinations returns combinations of n elements for a given s slice.
+//
+// Modified [version] to comply with requirements.
+//
+//   - return [][]T{} on empty combinations
+//   - return [][]T{} if n <= 0
+//
+// Original license: MIT
+// Copyright (c) 2018 Max Schmitt
+//
+// [version]: https://github.com/mxschmitt/golang-combinations/blob/main/combinations.go
+func Combinations[T any](s []T, n int) (ss [][]T) {
+	if n <= 0 {
+		return [][]T{}
+	}
+
+	l := uint(len(s))
+
+	// Iterate through all combinations of o
+	// from 1 (only first o in sbs) to 2^l length (all objects in sbs)
+	for ssb := 1; ssb < (1 << l); ssb++ {
+		if n > 0 && bits.OnesCount(uint(ssb)) != n {
+			continue
+		}
+
+		var sbs []T
+
+		for o := uint(0); o < l; o++ {
+			// checks if o is contained in sbs
+			// by checking if bit 'o' is set in ssb
+			if (ssb>>o)&1 == 1 {
+				// add o to sbs
+				sbs = append(sbs, s[o])
+			}
+		}
+		// add sbs to ss
+		ss = append(ss, sbs)
+	}
+	if len(ss) == 0 {
+		return [][]T{}
+	}
+	return ss
+}
---
+++ pkg/helpers/itertools/comb_test.go
@@ -0,0 +1,127 @@
+// Copyright Chainify Group LTD. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package itertools_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/chainifynet/aws-encryption-sdk-go/pkg/helpers/itertools"
+)
+
+func BenchmarkCombinationsNew(b *testing.B) {
+	type subtest struct {
+		name  string
+		slice []string
+		size  int
+	}
+	type test struct {
+		name string
+		ts   []subtest
+	}
+
+	var tests []test
+
+	for i := 1; i < 12; i++ {
+		tc := test{
+			name: fmt.Sprintf("slice %d", i),
+			ts:   make([]subtest, 0),
+		}
+		for j := 0; j < 10; j++ {
+			if j <= i+1 {
+				tc.ts = append(tc.ts, subtest{
+					name:  fmt.Sprintf("size %d", j),
+					slice: genSlice(i),
+					size:  j,
+				})
+			}
+		}
+		tests = append(tests, tc)
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			b.ReportAllocs()
+			b.ResetTimer()
+			for _, st := range tt.ts {
+				b.Run(st.name, func(b *testing.B) {
+					b.ReportAllocs()
+					b.ResetTimer()
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							itertools.Combinations(st.slice, st.size)
+						}
+					})
+				})
+			}
+		})
+	}
+}
+
+func genSlice(n int) (s []string) {
+	if n < 1 || n > 26 {
+		return nil
+	}
+	s = make([]string, n)
+	for i := 0; i < n; i++ {
+		s[i] = string(rune('a' + i))
+	}
+	return s
+}
+
+func TestCombinations(t *testing.T) {
+	type subtest struct {
+		name  string
+		slice []string
+		size  int
+	}
+	type test struct {
+		name string
+		ts   []subtest
+	}
+
+	var tests []test
+
+	for i := 1; i < 13; i++ {
+		tc := test{
+			name: fmt.Sprintf("slice %d", i),
+			ts:   make([]subtest, 0),
+		}
+		for j := 0; j < 11; j++ {
+			if j <= i+1 {
+				tc.ts = append(tc.ts, subtest{
+					name:  fmt.Sprintf("size %d", j),
+					slice: genSlice(i),
+					size:  j,
+				})
+			}
+		}
+		tests = append(tests, tc)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, st := range tt.ts {
+				t.Run(st.name, func(t *testing.T) {
+					res := itertools.Combinations(st.slice, st.size)
+					if st.size <= 0 {
+						assert.Equal(t, 0, len(res))
+					} else if st.size > len(st.slice) {
+						assert.Equal(t, 0, len(res))
+					} else if st.size == len(st.slice) {
+						assert.Equal(t, 1, len(res))
+					} else if st.size < len(st.slice) {
+						assert.Greater(t, len(res), st.size)
+					}
+
+					for _, v := range res {
+						assert.Equal(t, st.size, len(v))
+						assert.Subset(t, st.slice, v)
+					}
+				})
+			}
+		})
+	}
+}
