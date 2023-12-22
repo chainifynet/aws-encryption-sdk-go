eac79506b1c78f06d9028a3ab0ffa5a6d7f30b52
wobondar <wobondar@gmail.com>
2023-12-22 17:57:39 +0200

    feat(model): message header interfaces

---
+++ pkg/model/format/header.go
@@ -0,0 +1,52 @@
+// Copyright Chainify Group LTD. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package format
+
+import (
+	"github.com/chainifynet/aws-encryption-sdk-go/pkg/suite"
+)
+
+type Serializable interface {
+	Len() int
+	Bytes() []byte
+}
+
+type messageHeaderBase interface {
+	Version() suite.MessageFormatVersion
+	AlgorithmSuite() *suite.AlgorithmSuite
+	MessageID() []byte
+	AADLength() int
+	AADData() MessageAAD
+	EncryptedDataKeyCount() int
+	EncryptedDataKeys() []MessageEDK
+	ContentType() suite.ContentType
+	FrameLength() int
+}
+
+type MessageHeader interface {
+	Serializable
+	messageHeaderBase
+	Type() suite.MessageType    // present only in V1
+	Reserved() []byte           // present only in V1
+	IVLength() int              // present only in V1
+	AlgorithmSuiteData() []byte // present only in V2
+}
+
+type MessageEDK interface {
+	Serializable
+	ProviderID() string
+	ProviderInfo() string
+	EncryptedDataKey() []byte
+}
+
+type MessageAAD interface {
+	Serializable
+	EncryptionContext() suite.EncryptionContext
+}
+
+type MessageHeaderAuth interface {
+	Serializable
+	AuthData() []byte
+	IV() []byte // present only in V1
+}
