d86de45fed112bfff7983dfd5a943a86ee66c623
wobondar <wobondar@gmail.com>
2024-03-15 04:19:18 +0200

    refactor(utils): move itertools package under internal package

--- pkg/internal/utils/itertools/comb.go
+++ pkg/internal/utils/itertools/comb.go
@@ -1,7 +1,6 @@
 // Copyright Chainify Group LTD. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package itertools provides a method to generate all combinations out of a given string array.
 package itertools
 
 import "math/bits"
