15d68d40db9fa69cb0653a20bbcd2f218657151d
wobondar <wobondar@gmail.com>
2024-03-15 04:25:38 +0200

    docs: providers package documentation

--- pkg/providers/errors.go
+++ pkg/providers/errors.go
@@ -5,12 +5,28 @@ package providers
 
 import "errors"
 
-var (
-	ErrMasterKeyProvider                 = errors.New("MKP error")
-	ErrMasterKeyProviderDecrypt          = errors.New("MKP decrypt error")
-	ErrMasterKeyProviderDecryptForbidden = errors.New("MKP decrypt forbidden error")
-	ErrMasterKeyProviderEncrypt          = errors.New("MKP encrypt error")
-	ErrMasterKeyProviderNoPrimaryKey     = errors.New("MKP no primary key")
-	ErrConfig                            = errors.New("MKP config error")
-	ErrFilterKeyNotAllowed               = errors.New("MKP key not allowed by filter")
-)
+// ErrMasterKeyProvider is a generic error for master key provider.
+var ErrMasterKeyProvider = errors.New("MKP error")
+
+// ErrMasterKeyProviderDecrypt is returned when the master key provider fails to
+// decrypt.
+var ErrMasterKeyProviderDecrypt = errors.New("MKP decrypt error")
+
+// ErrMasterKeyProviderDecryptForbidden is returned when the master key provider
+// fails to decrypt due to forbidden access or filtering.
+var ErrMasterKeyProviderDecryptForbidden = errors.New("MKP decrypt forbidden error")
+
+// ErrMasterKeyProviderEncrypt is returned when the master key provider fails to
+// encrypt.
+var ErrMasterKeyProviderEncrypt = errors.New("MKP encrypt error")
+
+// ErrMasterKeyProviderNoPrimaryKey is returned when the master key provider has
+// no primary key.
+var ErrMasterKeyProviderNoPrimaryKey = errors.New("MKP no primary key")
+
+// ErrConfig is returned when the master key provider has a configuration error.
+var ErrConfig = errors.New("MKP config error")
+
+// ErrFilterKeyNotAllowed is returned when the master key provider has a key that
+// is not allowed by the filter.
+var ErrFilterKeyNotAllowed = errors.New("MKP key not allowed by filter")
